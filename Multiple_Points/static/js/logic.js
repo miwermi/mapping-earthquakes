b5c5b7b00986366fbdc021da967b475d594f1be0
Michelle Werner <mdwerner@iupui.edu>
2022-09-05 20:09:47 -0400

    Adding Multi-Points files

--- Multiple_Points/static/js/logic.js
+++ Multiple_Points/static/js/logic.js
@@ -10,7 +10,7 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
   console.log(city)
-  L.marker(city.location).addTo(map);
+  /*L.marker(city.location).addTo(map);*/
  });
 
 // We create the tile layer that will be the background of our map.
@@ -20,11 +20,24 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+//I prefer light (above) to dark (below
+// We create the tile layer that will be the background of our map.
+/*let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});)*/
+
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location)
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: 'orange',
+    lineweight: 4,
+
+})
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 .addTo(map);
 });
 
